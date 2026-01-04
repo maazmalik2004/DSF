22e2d1f558d236e6fe7a33cae99c313226f05f11
Maaz Malik <maazmalik2004@gmail.com>
2026-01-04 21:40:43 +0530

    [UPDATE] minor typing error in election service resolved

--- servicesLayer/localStorage/localStorage.js
+++ servicesLayer/localStorage/localStorage.js
@@ -1,20 +1,55 @@
-import fs from "fs"
+// import fs from "fs"
 
-class LocalStorage{
-    constructor(object){
-        this.basePath = "./servicesLayer/localStorage/" + (object.identifier||"DEFAULT")
-        if(!fs.existsSync(this.basePath))fs.mkdirSync(this.basePath);
+// class LocalStorage{
+//     constructor(object){
+//         this.basePath = "./servicesLayer/localStorage/" + (object.identifier||"DEFAULT")
+//         if(!fs.existsSync(this.basePath))fs.mkdirSync(this.basePath);
+//     }
+
+//     get(key){
+//         let path = this.basePath+"/"+key;
+//         if(!fs.existsSync(path))return null
+//         let value = fs.readFileSync(path);
+//         return JSON.parse(value);
+//     }
+
+//     set(key, value){
+//         fs.writeFileSync(this.basePath+"/"+key, JSON.stringify(value,null,4));
+//     }
+// }
+
+// export default LocalStorage;
+
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+class LocalStorage {
+    constructor(object = {}) {
+        this.basePath = path.join(
+            __dirname,
+            "servicesLayer",
+            "localStorage",
+            object.identifier || "DEFAULT"
+        );
+
+        fs.mkdirSync(this.basePath, { recursive: true });
     }
 
-    get(key){
-        let path = this.basePath+"/"+key;
-        if(!fs.existsSync(path))return null
-        let value = fs.readFileSync(path);
+    get(key) {
+        const filePath = path.join(this.basePath, key);
+        if (!fs.existsSync(filePath)) return null;
+
+        const value = fs.readFileSync(filePath, "utf-8");
         return JSON.parse(value);
     }
 
-    set(key, value){
-        fs.writeFileSync(this.basePath+"/"+key, JSON.stringify(value,null,4));
+    set(key, value) {
+        const filePath = path.join(this.basePath, key);
+        fs.writeFileSync(filePath, JSON.stringify(value, null, 4));
     }
 }
 
